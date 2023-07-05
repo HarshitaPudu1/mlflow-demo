23aac554a86a3220f5fa33aaab150ea06e313f8b
HarshitaPudu1 <133955678+HarshitaPudu1@users.noreply.github.com>
2023-07-05 12:08:53 +0530

    Update validate_and_combine.py

--- mlflowBlobTrigger/validate_and_combine.py
+++ mlflowBlobTrigger/validate_and_combine.py
@@ -1,12 +1,17 @@
 import pandas as pd
 import argparse
-import os.path
-import datetime
+
 
 def validate_csv(file_path):
     # Load CSV file into a DataFrame
     df = pd.read_csv(file_path, encoding='utf8')
+    # Check column headers
+    expected_columns = ["MANAGER_ID"] 
+    if not set(expected_columns).issubset(df.columns):
+        raise ValueError("Invalid column headers")
+    
     return df
+
 
 def combine_csv(input1, input2, output):
     # Perform data validation on both input files
@@ -19,6 +24,7 @@ def combine_csv(input1, input2, output):
     # Save combined data as Parquet
     combined_data.to_parquet(output, index=False)
 
+
 def main():
     # Parse command-line arguments
     parser = argparse.ArgumentParser()
@@ -26,10 +32,10 @@ def main():
     parser.add_argument("--input2", type=str, help="Path to input CSV file 2")
     parser.add_argument("--output", type=str, help="Path to output Parquet file")
     args = parser.parse_args()
-    print(args.input1)
 
     # Call combine_csv function with input and output paths
     combine_csv(args.input1, args.input2, args.output)
+
 
 if __name__ == "__main__":
     main()
